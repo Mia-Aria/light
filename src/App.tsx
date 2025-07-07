a6e904bd666ec7617cf77cd20997447a0bc759ed
gaoyang334 <gaoyang334@jd.com>
2025-07-07 16:53:10 +0800

    fix: renew

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: gaoyang334 gaoyang334@jd.com
  * @Date: 2025-07-03 11:01:33
  * @LastEditors: gaoyang334 gaoyang334@jd.com
- * @LastEditTime: 2025-07-03 21:03:51
+ * @LastEditTime: 2025-07-07 16:52:38
  * @FilePath: /light/src/App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,47 +11,100 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [input, setInput] = React.useState<number>(1);
+  const [input, setInput] = React.useState<string>('');
   const [data, setData] = React.useState<any>('');
-  const fetchData = async (key: number) => {
-    if(typeof key !== 'number') {
-      return;
+  const [list, setList] = React.useState<any>('');
+
+  const path = process.env.NODE_ENV === 'production' ? 'http://182.92.182.238:4000' : 'http://localhost:4000';
+  const fetchList = async () => {
+    try{
+      await fetch(`${path}/api/users`).then(res => {
+        return res.json();
+      }).then(
+        res => {
+          setList(res);
+        }
+      )
+    }catch(err){
+      console.log(err, 'fetchList')
     }
-    await fetch(`http://182.92.182.238:4000/api/users/${key}`).then(res => {
-      return res.json();
-    }).then(
-      res => {
-        setData(res);
-      }
-    )
+  }
+  const fetchData = async (key: string) => {
+    try{
+      await fetch(`${path}/api/users/${key}`).then(res => {
+        return res.json();
+      }).then(
+        res => {
+          setData(res);
+        }
+      )
+    }catch(err){
+      console.log(err, 'fetchData')
+    }
   }
 
   React.useEffect(() => {
     fetchData(input);
   }, [input]);
 
+  React.useEffect(() => {
+    fetchList();
+  }, []);
+
+  // excel to json
+  const [json, setJson] = React.useState<any>('');
+  async function convertToJson() {
+    const fileInput = document.getElementById('fileInput')  as HTMLInputElement;
+    if (!fileInput) return;
+    const file = fileInput?.files?.[0] as File;
+    if (!file) return;
+    const formData = new FormData();
+    formData.append('excelFile', file);
+
+    try {
+      setJson('converting...');
+      
+      const response = await fetch('http://localhost:4000/api/excel-to-json', {
+        method: 'POST',
+        body: formData
+      });
+
+      if (!response.ok) {
+        throw new Error('Conversion failed');
+      }
+
+      const data = await response.json();
+      setJson(JSON.stringify(data));
+    } catch (error) {
+      setJson('Error');
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2>Dark in Light Utils</h2>
         <p>
-          Dark in Light
+          Please input a name~
         </p>
-        <p>
-          Please input a number~
-        </p>
-        <input type="number" value={input} onChange={(e) => setInput(Number(e.target.value))} />
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
         <p>
           {JSON.stringify(data)}  
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn DarkinLight
-        </a>
+        ====================================================================================
+        <p>
+          All users
+        </p>
+        <p>
+          {JSON.stringify(list)}  
+        </p>
+        ====================================================================================
+        <h2>Convert Excel to JSON</h2>
+        <input type="file" id="fileInput" accept=".xlsx,.xls,.csv" />
+        <button onClick={() => convertToJson()}>Convert to JSON</button>
+        <br />
+        <div id="result">{json}</div>
       </header>
     </div>
   );
